1a9d40868f209b89737972a6b8db8e2ba690186e
Dada Khalandar <kerry.blig12@gmail.com>
2025-10-25 13:39:06 +0700

    refactor: simplify App component and update styles in index.css

--- src/App.tsx
+++ src/App.tsx
@@ -5,34 +5,15 @@ import "./App.css";
 import { Button } from "./components/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+    <main className='min-h-screen font-noto-sans'>
+      <div className='container mx-auto px-4 py-8'>
+        <header className='mb-8 text-center'>
+          <h1 className='text-4xl font-bold text-yellow-500 mb-2'>Pokédex</h1>
+          <p className='text-muted-foreground'>Discover and explore Pokémon</p>
+        </header>
       </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <Button
-          className='active:scale-90 visited:scale-90'
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   );
 }
 
